3dc576928e33afa192e9343ae8ba766164462232
Ryan Caltabiano <rcaltabiano90@gmail.com>
2023-10-25 17:58:45 -0400

    edited code to tweak format

--- Develop/index.js
+++ Develop/index.js
@@ -2,13 +2,13 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 
 const licenses = [
-  { name: 'MIT_License', value: 'MIT', url: 'https://opensource.org/licenses/MIT', badge: 'MIT'},
-  { name: 'BSD_2-Clause_"Simplified"_License', value: 'BSD-2-Clause', url: 'https://opensource.org/licenses/BSD-2-Clause', badge: 'BSD--2--Clause'},
-  { name: 'BSD 3-Clause_"New"_or_"Revised"_License', value: 'BSD-3-Clause', url: 'https://opensource.org/licenses/BSD-3-Clause', badge: 'BSD--3--Clause'},
-  { name: 'GNU_GPLv3', value: 'GPL-3.0', url: 'https://www.gnu.org/licenses/gpl-3.0.en.html', badge: 'GPL--3.0'},
-  { name: 'Mozilla_Public_License 2.0', value: 'MPL-2.0', url: 'https://opensource.org/licenses/MPL-2.0', badge: 'MPL--2.0'},
-  { name: 'The_Unlicense', value: 'unlicense', url: 'http://unlicense.org/', badge: 'unlicense'},
-  { name: 'Apache_License_2.0', value: 'Apache-2.0', url: 'https://opensource.org/licenses/Apache-2.0', badge:'Apache--2.0'},
+  { name: 'MIT License', value: 'MIT', url: 'https://opensource.org/licenses/MIT', badge: 'MIT'},
+  { name: 'BSD 2-Clause "Simplified" License', value: 'BSD-2-Clause', url: 'https://opensource.org/licenses/BSD-2-Clause', badge: 'BSD--2--Clause'},
+  { name: 'BSD 3-Clause "New" or "Revised" License', value: 'BSD-3-Clause', url: 'https://opensource.org/licenses/BSD-3-Clause', badge: 'BSD--3--Clause'},
+  { name: 'GNU GPLv3', value: 'GPL-3.0', url: 'https://www.gnu.org/licenses/gpl-3.0.en.html', badge: 'GPL--3.0'},
+  { name: 'Mozilla Public License 2.0', value: 'MPL-2.0', url: 'https://opensource.org/licenses/MPL-2.0', badge: 'MPL--2.0'},
+  { name: 'The Unlicense', value: 'unlicense', url: 'http://unlicense.org/', badge: 'unlicense'},
+  { name: 'Apache License 2.0', value: 'Apache-2.0', url: 'https://opensource.org/licenses/Apache-2.0', badge:'Apache--2.0'},
 ];
 
 const init = () => {
@@ -65,7 +65,7 @@ const init = () => {
       const selectedLicense = licenses.find((license) => license.value === answers.license);
       let readmeContent = `# ${answers.title}
 
-      [![License: ${selectedLicense.name}](https://img.shields.io/badge/License-${selectedLicense.badge}-yellow.svg)](${selectedLicense.url})
+    ![License: ${selectedLicense.name}(https://img.shields.io/badge/License-${selectedLicense.badge}-yellow.svg)](${selectedLicense.url})
 
 ## Table of Contents
 
